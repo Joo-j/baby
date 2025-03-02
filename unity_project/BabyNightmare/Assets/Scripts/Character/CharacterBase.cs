ca7ea5ba487ee1049db892e104bc9d0ee7bab59c
sc-choppa <chopper@supercent.io>
2025-03-03 00:18:18 +0900

    HP 바 위치 수동 지정

--- unity_project/BabyNightmare/Assets/Scripts/Character/CharacterBase.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Character/CharacterBase.cs
@@ -11,10 +11,11 @@ namespace BabyNightmare.Character
         public float Health { get; }
     }
 
-    public abstract class CharacterBase : MonoBehaviour
+    public abstract class CharacterBase : BehaviourBase
     {
         [SerializeField] protected Animator _animator;
         [SerializeField] protected MeshRenderer _renderer;
+        [SerializeField] private Transform _hpTF;
 
         private const string PATH_SIMPLE_PROGRESS = "Util/SimpleProgress";
         protected const string PATH_PROJECTILE = "Inventory/Projectile";
@@ -30,8 +31,7 @@ namespace BabyNightmare.Character
         {
             _currentHealth = _maxHealth = context.Health;
 
-            _hpBar = ObjectUtil.LoadAndInstantiate<SimpleProgress>(PATH_SIMPLE_PROGRESS, transform);
-            _hpBar.transform.localPosition = new Vector3(0, 1.4f, 0);
+            _hpBar = ObjectUtil.LoadAndInstantiate<SimpleProgress>(PATH_SIMPLE_PROGRESS, _hpTF);
             _hpBar.transform.rotation = Quaternion.Euler(0, 0, 0);
             _hpBar.Refresh(_currentHealth, _maxHealth, true);
         }
@@ -51,4 +51,3 @@ namespace BabyNightmare.Character
         }
     }
 }
-
