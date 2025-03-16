0ae6154ec77fd890e96ab25d3eeb8d72d509f073
sc-choppa <chopper@supercent.io>
2025-03-16 18:46:08 +0900

    인게임 UI 적용 작업 #1

--- unity_project/BabyNightmare/Assets/Scripts/Util/SimpleProgress.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Util/SimpleProgress.cs
@@ -3,32 +3,31 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Supercent.Util;
 
 namespace BabyNightmare.Util
 {
     public class SimpleProgress : MonoBehaviour
     {
-        [SerializeField] private Image _bgIMG;
+        [SerializeField] private RectTransform _rtf;
         [SerializeField] private Image _progressIMG;
         [SerializeField] private TextMeshProUGUI _progressTMP;
         [SerializeField] private float _fillDuration = 1f;
 
         private Coroutine _coFill = null;
-        private RectTransform _progressRTF = null;
-        private Vector2 _bgSize = default;
+        private Vector2 _progressSize;
 
         private void Awake()
         {
-            _progressRTF = _progressIMG.rectTransform;
-            _bgSize = _bgIMG.rectTransform.rect.size;
+            _progressSize = _progressIMG.rectTransform.rect.size;
         }
 
         public void Refresh(float current, float max, bool immediate)
         {
             if (true == immediate)
             {
-                _progressTMP.text = $"{current}/ {max}";
-                _progressRTF.sizeDelta = new Vector2(Mathf.Lerp(0, _bgSize.x, current / max), _bgSize.y);
+                _progressTMP.text = $"{current}";
+                _progressIMG.rectTransform.sizeDelta = new Vector2(Mathf.Lerp(0, _progressSize.x, current / max), _progressSize.y);
                 return;
             }
 
@@ -41,18 +40,18 @@ namespace BabyNightmare.Util
         private IEnumerator Co_Fill(float current, float max)
         {
             var elapsed = 0f;
-            var startSize = _progressRTF.sizeDelta;
-            var targetSize = new Vector2(Mathf.Lerp(0, _bgSize.x, current / max), _bgSize.y);
+            var startSize = _progressIMG.rectTransform.sizeDelta;
+            var targetSize = new Vector2(Mathf.Lerp(0, _progressSize.x, current / max), _progressSize.y);
             while (elapsed < _fillDuration)
             {
                 yield return null;
                 elapsed += Time.deltaTime;
                 var factor = elapsed / _fillDuration;
-                _progressRTF.sizeDelta = Vector2.Lerp(startSize, targetSize, factor);
+                _progressIMG.rectTransform.sizeDelta = Vector2.Lerp(startSize, targetSize, factor);
             }
 
-            _progressRTF.sizeDelta = targetSize;
-            _progressTMP.text = $"{current} / {max}";
+            _progressIMG.rectTransform.sizeDelta = targetSize;
+            _progressTMP.text = $"{current}";
 
             _coFill = null;
         }
