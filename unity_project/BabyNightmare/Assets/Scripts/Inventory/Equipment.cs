25bbf8b9a29e86734ddf93f29c33eb01b916cd40
sc-choppa <chopper@supercent.io>
2025-03-17 01:44:52 +0900

    장비 리소스 로드 구조 개선

--- unity_project/BabyNightmare/Assets/Scripts/Inventory/Equipment.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Inventory/Equipment.cs
@@ -12,13 +12,16 @@ namespace BabyNightmare.InventorySystem
     public class Equipment : MonoBehaviour
     {
         [SerializeField] private RectTransform _rtf;
-        [SerializeField] private Image _image;
+        [SerializeField] private Transform _fxTF;
+        [SerializeField] private Image _iconOutline;
+        [SerializeField] private Image _icon;
         [SerializeField] private Image _coolImage;
         [SerializeField] private AnimationCurve _moveCurve;
         [SerializeField] private AnimationCurve _swingCurve;
 
-        private const string PATH_EQUIPMENT_FX = "Inventory/FX/FX_Equipment_";
-        private const string PATH_EQUIPMENT_MERGE_FX = "Inventory/FX/FX_Merge";
+        private const string PATH_EQUIPMENT_ICON_OUTLINE = "Inventory/Equipment_Icon_Outline/";
+        private const string PATH_EQUIPMENT_ICON = "Inventory/Equipment_Icon/";
+        private const string PATH_EQUIPMENT_FX = "Inventory/FX/FX_";
         private Coroutine _coCoolDown = null;
         private Coroutine _coSwing = null;
         private ParticleSystem _fx = null;
@@ -31,9 +34,19 @@ namespace BabyNightmare.InventorySystem
         {
             this.Data = data;
 
-            _image.sprite = data.Sprite;
-            _image.SetNativeSize();
-            _image.raycastTarget = false;
+            var iconOutlinePath = $"{PATH_EQUIPMENT_ICON_OUTLINE}{data.Name}_Outline";
+            var icon = Resources.Load<Sprite>(iconOutlinePath);
+
+            _rtf.sizeDelta = icon.rect.size;
+
+            _iconOutline.sprite = icon;
+            Debug.Assert(null != _iconOutline.sprite, $"{iconOutlinePath} | no outline icon");
+
+            var iconPath = $"{PATH_EQUIPMENT_ICON}{data.Name}";
+            _icon.sprite = Resources.Load<Sprite>(iconPath);
+            Debug.Assert(null != _iconOutline.sprite, $"{iconPath} | no icon");
+
+
             transform.localScale = Vector3.one;
 
             if (null != _fx)
@@ -42,16 +55,20 @@ namespace BabyNightmare.InventorySystem
                 _fx = null;
             }
 
-            var fxRes = Resources.Load<ParticleSystem>($"{PATH_EQUIPMENT_FX}{data.ID}");
+            var fxPath = $"{PATH_EQUIPMENT_FX}{data.Name}_LV{data.Level}";
+            var fxRes = Resources.Load<ParticleSystem>(fxPath);
             if (null != fxRes)
             {
-                _fx = Instantiate(fxRes);
+                _fx = Instantiate(fxRes, _fxTF);
+                _fx.transform.localPosition = Vector3.zero;
             }
 
             if (true == isMerge)
             {
-                var mergeFX = ObjectUtil.LoadAndInstantiate<ParticleSystem>(PATH_EQUIPMENT_MERGE_FX, transform);
-                mergeFX.transform.localScale = Vector3.one * 20;
+                var mergeFxRes = Resources.Load<ParticleSystem>($"{PATH_EQUIPMENT_FX}Merge");
+                var mergeFX = Instantiate(mergeFxRes, transform);
+                mergeFX.transform.localPosition = Vector3.zero;
+
                 StartCoroutine(SimpleLerp.Co_Invoke(2, () => Destroy(mergeFX.gameObject)));
             }
         }
