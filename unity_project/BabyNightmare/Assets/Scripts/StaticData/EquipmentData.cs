8fd0dd0b895093371f394c25c3e5c4e81aa7d899
sc-choppa <chopper@supercent.io>
2025-03-20 18:07:35 +0900

    장비 데이터 스탯 데이터 리스트 구조로 변경

--- unity_project/BabyNightmare/Assets/Scripts/StaticData/EquipmentData.cs
+++ unity_project/BabyNightmare/Assets/Scripts/StaticData/EquipmentData.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using BabyNightmare.Util;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
+using System;
 
 namespace BabyNightmare.StaticData
 {
+    [Serializable]
+    public struct StatData
+    {
+        public EStatType Type;
+        public float Value;
+    }
+
     [CreateAssetMenu(fileName = "EquipmentData", menuName = "BabyNightmare/EquipmentData")]
     public class EquipmentData : ScriptableObject
     {
@@ -11,10 +20,7 @@ namespace BabyNightmare.StaticData
         public string Name;
         public string Desc;
         public int Level;
-        public float Damage;
-        public float Heal;
-        public float Defence;
-        public int Coin;
+        public List<StatData> StatDataList;
         public float CoolTime;
         public Sprite Sprite;
         public RectShape Shape;
