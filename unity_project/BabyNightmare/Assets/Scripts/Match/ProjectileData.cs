b4e89b03b32d1d01c0f9ddf01cd3e6a799d2f865
SC-choppa <choppa@supercent.io>
2025-03-25 18:51:08 +0900

    투사체 스크립트 네임스페이스 적용

--- unity_project/BabyNightmare/Assets/Scripts/Match/ProjectileData.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Match/ProjectileData.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ProjectileData", menuName = "BabyNightmare/ProjectileData")]
-public class ProjectileData : ScriptableObject
+namespace BabyNightmare.StaticData
 {
-    [SerializeField] private Mesh _mesh;
-    [SerializeField] private Material[] _materials;
-    [SerializeField] private float _throwDuration;
-    [SerializeField] private AnimationCurve _throwCurve;
-    [SerializeField] private float _bezierHeight;
-    [SerializeField] private float _targetAngleZ;
+    [CreateAssetMenu(fileName = "ProjectileData", menuName = "BabyNightmare/ProjectileData")]
+    public class ProjectileData : ScriptableObject
+    {
+        [SerializeField] private Mesh _mesh;
+        [SerializeField] private Material[] _materials;
+        [SerializeField] private float _throwDuration;
+        [SerializeField] private AnimationCurve _throwCurve;
+        [SerializeField] private float _bezierHeight;
+        [SerializeField] private float _targetAngleZ;
 
-    public Mesh Mesh => _mesh;
-    public Material[] Materials => _materials;
-    public float Duration => _throwDuration;
-    public AnimationCurve Curve => _throwCurve;
-    public float BezierHeight => _bezierHeight;
-    public float TargetAngleZ => _targetAngleZ;
+        public Mesh Mesh => _mesh;
+        public Material[] Materials => _materials;
+        public float Duration => _throwDuration;
+        public AnimationCurve Curve => _throwCurve;
+        public float BezierHeight => _bezierHeight;
+        public float TargetAngleZ => _targetAngleZ;
+    }
 }
