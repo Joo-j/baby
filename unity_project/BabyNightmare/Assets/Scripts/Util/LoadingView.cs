a28d8598b795e3d4f9e9198c3b7a29bbaa5deaba
SC-choppa <choppa@supercent.io>
2025-03-27 00:46:35 +0900

    로딩뷰 프로그레스 제거

--- unity_project/BabyNightmare/Assets/Scripts/Util/LoadingView.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Util/LoadingView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Supercent.Util;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,38 +12,18 @@ namespace BabyNightmare.Util
     public class LoadingView : MonoBehaviour
     {
         [SerializeField] private Image _bgIMG;
-        [SerializeField] private Image _progressIMG;
-        [SerializeField] private TextMeshProUGUI _progresTMP;
-        [SerializeField] private float _duration = 10f;
-        [SerializeField] private AnimationCurve _curve;
         [SerializeField] private Sprite[] _bgResArr;
+        [SerializeField] private float _duration = 2f;
 
         public void Init(Action doneCallback)
         {
             _bgIMG.sprite = _bgResArr[Random.Range(0, _bgResArr.Length)];
-            _progressIMG.fillAmount = 0f;
-            _progresTMP.text = "0/100";
-
             StartCoroutine(Co_Loading(doneCallback));
         }
 
         IEnumerator Co_Loading(Action doneCallback)
         {
-            var elapsed = 0f;
-            while (elapsed < _duration)
-            {
-                elapsed += Time.deltaTime;
-                yield return null;
-
-                var factor = _curve.Evaluate(elapsed / _duration);
-                _progressIMG.fillAmount = factor;
-                var progress = factor * 100f;
-                _progresTMP.text = $"{progress:F1}/100";
-            }
-
-            _progressIMG.fillAmount = 1f;
-            _progresTMP.text = "100/100";
-
+            yield return CoroutineUtil.WaitForSeconds(_duration);
             doneCallback?.Invoke();
             Destroy(gameObject);
         }
