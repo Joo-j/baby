eccca89c9f7dd9f5e3c11b2e42d64159d584e804
Green <green@supercent.io>
2025-03-26 17:11:31 +0900

    배경 에셋 추가

---
+++ unity_project/BabyNightmare/Assets/AllIn1VfxToolkit/Demo & Assets/Demo/Scripts/AllIn1DoShake.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace AllIn1VfxToolkit.Demo.Scripts
+{
+    public class AllIn1DoShake : MonoBehaviour
+    {
+        [SerializeField] private float _shakeAmount = 0.15f;
+        [SerializeField] private bool doShakeOnStart;
+        [SerializeField] private float shakeOnStartDelay;
+        
+        private void Update() {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                DoShake();
+            }
+        }
+
+        public void DoShake()
+        {
+            DoShake(_shakeAmount);
+        }
+        public void DoShake(float shakeAmount)
+        {
+            AllIn1Shaker.i.DoCameraShake(shakeAmount);
+        }
+    }
+}
