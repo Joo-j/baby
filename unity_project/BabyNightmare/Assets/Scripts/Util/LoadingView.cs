69c9f97efc032b9e8f0aa35cc5f78c7ad3c22a6f
SC-choppa <choppa@supercent.io>
2025-03-27 18:42:57 +0900

    로딩창 페이드인아웃

--- unity_project/BabyNightmare/Assets/Scripts/Util/LoadingView.cs
+++ unity_project/BabyNightmare/Assets/Scripts/Util/LoadingView.cs
@@ -11,19 +11,26 @@ namespace BabyNightmare.Util
 {
     public class LoadingView : MonoBehaviour
     {
+        [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private Image _bgIMG;
         [SerializeField] private Sprite[] _bgResArr;
         [SerializeField] private float _duration = 2f;
 
         public void Init(Action doneCallback)
         {
+            _canvasGroup.alpha = 0f;
             _bgIMG.sprite = _bgResArr[Random.Range(0, _bgResArr.Length)];
             StartCoroutine(Co_Loading(doneCallback));
         }
 
         IEnumerator Co_Loading(Action doneCallback)
         {
+            yield return SimpleLerp.Co_LerpAlpha(_canvasGroup, 0, 1f, 0.2f, CurveHelper.Preset.Linear);
+
             yield return CoroutineUtil.WaitForSeconds(_duration);
+            
+            yield return SimpleLerp.Co_LerpAlpha(_canvasGroup, 1f, 0f, 0.2f, CurveHelper.Preset.Linear);
+
             doneCallback?.Invoke();
             Destroy(gameObject);
         }
